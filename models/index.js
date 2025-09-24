a320794ad584d3670d9c3e74cc98d6490be17c39
Bonnie <bonnie@bonnie.org>
2025-09-24 22:03:23 +0300

    Fix model imports for Render

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Role = require('./Role');
+const Ticket = require('./Ticket');
+const Message = require('./Message');
+
+module.exports = {
+  User,
+  Role,
+  Ticket,
+  Message,
+};
