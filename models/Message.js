365f2938843860bac2f4f6c61631c0b357b7a2f2
Bonnie <bonnie@bonnie.org>
2025-09-26 22:51:08 +0300

    Fix: correct Sequelize config import path in Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,20 +1,13 @@
 // src/models/Message.js
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const sequelize = require("../config/db");  // ✅ fixed path
 
 const Message = sequelize.define("Message", {
-  ticketId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  senderId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  }
+  ticketId: { type: DataTypes.INTEGER, allowNull: false },
+  senderId: { type: DataTypes.INTEGER, allowNull: false },
+  content: { type: DataTypes.TEXT, allowNull: false },
+}, {
+  timestamps: true,
 });
 
 module.exports = Message;
